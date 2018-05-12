e3762f13dc4d8f78b373496281a5139974ee6d55
Ben Verna-Asals <benjamin.asals@gmail.com>
2018-05-12 18:30:27 -0400

    found words that have matches in emoji table (#17)
    * lil reorganizing
    
    * found words that have emoji matches
    
    * made findMatches function
    
    * organized

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,13 @@
 module Lib ( main ) where
 
 import qualified String
-import qualified Emoji
 
 main :: IO ()
 main = do
   putStrLn "Type a sentence that you want to emojify:"
   sentence <- getLine
-  -- let keywords = String.clean sentence
-  -- print $ map (`elem` Emoji.table) keywords
-  let emojified = String.addEmoji "^" (String.splitWords sentence)
-  print $ unwords emojified
+  let keywords = String.clean sentence
+  let matches = String.findMatches keywords
+  print matches
+  -- let emojified = unwords $ String.addEmoji "^" (String.splitWords sentence)
+  -- print emojified
--- src/String.hs
+++ src/String.hs
@@ -1,8 +1,9 @@
-module String ( addEmoji, clean, returnMatch, splitWords ) where
+module String ( addEmoji, clean, contains, findMatches, splitWords ) where
 
 import qualified Data.List.Split
 import qualified Data.Char
 import qualified Data.List
+import qualified Emoji
 
 addEmoji :: String -> [String] -> [String]
 addEmoji emoji [] = []
@@ -11,24 +12,27 @@ addEmoji emoji (x:y) = x : emoji : addEmoji emoji y
 clean :: String -> [String]
 clean = map (lowerCase . removePunc) . splitWords
 
-returnMatch :: String -> String -> String
-returnMatch word emoji = if containsString word emoji then ":" ++ emoji ++ ":" else "no match"
+contains :: String -> String -> Bool
+contains = Data.List.isInfixOf
+
+-- findEmoji :: String -> String -> String
+-- findEmoji word emoji = if word `contains` emoji then ":" ++ emoji ++ ":" else ""
+
+findMatches :: [String] -> [String]
+findMatches keywords = Data.List.intersectBy Data.List.isInfixOf keywords Emoji.table
 
 splitWords :: String -> [String]
 splitWords = Data.List.Split.splitOn " "
 
 --
 
-specialChars :: String
-specialChars = ",.?!-:;\"\'()=*&^%$#@`~[]{}\\/<>_+"
-
---
-
 lowerCase :: String -> String
 lowerCase = map Data.Char.toLower
 
-containsString :: String -> String -> Bool
-containsString = Data.List.isInfixOf
-
 removePunc :: String -> String
 removePunc word = [ clean | clean <- word, clean `notElem` specialChars ]
+
+--
+
+specialChars :: String
+specialChars = ",.?!-:;\"\'()=*&^%$#@`~[]{}\\/<>_+"
