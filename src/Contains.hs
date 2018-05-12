860596dc2dacd0d8b806e9c6c16f03a966df3774
Etio Tan <etiotan@gmail.com>
2018-05-11 23:08:26 -0400

    Et contains string (#11)
    * can see if a string, contains a substring, eskeddddit
    
    * we can just test in ghci!

---
+++ src/Contains.hs
@@ -0,0 +1,13 @@
+module Contains (containsString) where
+
+import qualified Data.List
+
+
+-- isInfixOf 'hello' 'hello world' smaller word first, emoji names are longer
+-- containsString word emoji = isInfixOf word emoji <-- is identical
+-- containsString = isInfixOf  <-- is identical but hard to read
+
+containsString :: String -> String -> Bool
+
+containsString word emoji =  word `Data.List.isInfixOf` emoji
+
